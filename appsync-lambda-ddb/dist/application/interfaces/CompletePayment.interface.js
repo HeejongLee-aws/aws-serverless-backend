8ff2f023bbdf3a4e7bb9e9621bfec65d3e5ca959
isheejong <isheejong@gmail.com>
2020-09-22 01:06:36 +0900

    update lambda function

--- appsync-lambda-ddb/dist/application/interfaces/CompletePayment.interface.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CompletePayment = void 0;
-var CompletePayment = /** @class */ (function () {
-    function CompletePayment() {
-    }
-    return CompletePayment;
-}());
-exports.CompletePayment = CompletePayment;
